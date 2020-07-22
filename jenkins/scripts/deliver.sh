9f55aeb039c4858f7e2627c807e565759e3ee281
Jefferson Felisberto Januario <jefferson.januario@cassol.local>
2020-07-22 13:34:08 -0300

    Revert "Revert "Feito ajustes das bibliotecas, logs do terminal e informação de hostname no build""
    This reverts commit a8bf40f434951d21c4b3f2fb1a2eb4f2950de114.

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -1,29 +1,29 @@
-#!/usr/bin/env sh
+#!/usr/bin/env sh 
+echo 'The following "npm" command builds your Node.js/React application for'> output.txt
+echo 'production in the local "build" directory (i.e. within the'>> output.txt
+echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'>> output.txt
+echo 'correctly bundles React in production mode and optimizes the build for'>> output.txt
+echo 'the best performance.'>> output.txt
+set -x  >> output.txt
+npm run build >> output.txt
+set +x >> output.txt
 
-echo 'The following "npm" command builds your Node.js/React application for'
-echo 'production in the local "build" directory (i.e. within the'
-echo '"/var/jenkins_home/workspace/simple-node-js-react-app" directory),'
-echo 'correctly bundles React in production mode and optimizes the build for'
-echo 'the best performance.'
-set -x
-npm run build
-set +x
+echo 'The following "npm" command runs your Node.js/React application in'>> output.txt
+echo 'development mode and makes the application available for web browsing.'>> output.txt
+echo 'The "npm start" command has a trailing ampersand so that the command runs'>> output.txt
+echo 'as a background process (i.e. asynchronously). Otherwise, this command'>> output.txt
+echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'>> output.txt
+echo 'is followed by another command that retrieves the process ID (PID) value'>> output.txt
+echo 'of the previously run process (i.e. "npm start") and writes this value to'>> output.txt
+echo 'the file ".pidfile".'>> output.txt
+set -x >> output.txt
+npm start & >> output.txt
+sleep 1 >> output.txt
+echo $! > .pidfile >> output.txt
+set +x >> output.txt
 
-echo 'The following "npm" command runs your Node.js/React application in'
-echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
-echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
-echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
-echo 'the file ".pidfile".'
-set -x
-npm start &
-sleep 1
-echo $! > .pidfile
-set +x
+echo 'Now...' >> output.txt
+echo 'Visit http://'$HOSTNAME':3000 to see your Node.js/React application in action.'>> output.txt
+echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'>> output.txt
+echo 'created your initial Pipeline as a Jenkinsfile.)'>> output.txt
 
-echo 'Now...'
-echo 'Visit http://192.168.1.7:3000 to see your Node.js/React application in action.'
-echo '(This is why you specified the "args ''-p 3000:3000''" parameter when you'
-echo 'created your initial Pipeline as a Jenkinsfile.)'
